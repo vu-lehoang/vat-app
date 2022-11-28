5b4430165c6b22b856d05b9174c4d7b7a59b50bc
ACER <ACER@DESKTOP-I244P3O>
2022-11-28 16:42:44 +0700

    update

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Vat Calculator</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="/image/logo_favicon.png>
 </head>
 <?php
 /**
